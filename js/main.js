8a0fc2cb2fb1885f6f71909f0a5a9b95b2939f67
Tanya Grunicheva <eiforiaa.2010@mail.ru>
2020-10-14 00:14:21 +0300

    все работает

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	$('.header__burger').click(function(event){
-		$('header__burger,.header__menu').toggleClass('active');
-
+		$('.header__menu').toggleClass('active');
+		$('.header__burger').toggleClass('active');
+		$('body').toggleClass('lock');
 	});
-	// 
+	
 });
